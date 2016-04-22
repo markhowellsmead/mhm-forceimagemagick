55d15e359f69f839a8a4d2555b7e5158ca713189
Mark Howells-Mead <mark@permanenttourist.ch>
2016-04-22 12:21:52 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: Force ImageMagick
+Plugin URI: https://github.com/mhmli/mhm-forceimagemagick/
+Description: Forces WordPress to use ImageMagick instead of the PHP GD image library. This allows EXIF data to be retained, amongst other things.
+Author: Mark Howells-Mead
+Version: 1.0
+Author URI: http://permanenttourist.ch/
+*/
+
+class MHMForceImageMagick {
+
+    public $key     = '';
+    public $version = '1.0';
+
+    public function __construct(){
+        $this->key = basename(__DIR__);
+        add_filter('wp_image_editors', array($this, 'fi_force_imagick') );
+    }
+    
+    public function fi_force_imagick() {
+        return array('WP_Image_Editor_Imagick');
+    }
+
+}
+
+new MHMForceImageMagick();
